70280a81df6ebf3508c090f9c570914e44a585a8
Jaskaran Singh <jaskaranrehal@gmail.com>
2020-03-30 23:39:58 +0530

    Migrations fixed

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date publish')
-    updated_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
         return self.question_text
